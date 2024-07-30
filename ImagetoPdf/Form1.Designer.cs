6e1a73a525e0f1afa99df0a2c837c963d5d8d1f1
MaharishiDavey <21it025@charusat.edu.in>
2024-07-30 11:27:59 +0530

    Add project files.

---
+++ ImagetoPdf/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ImagetoPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.ManuallyPanel = new System.Windows.Forms.Panel();
+            this.opacityValue = new System.Windows.Forms.TextBox();
+            this.OpacityValueLable = new System.Windows.Forms.Label();
+            this.logoImage = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.AddLogoBox = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.ManuallyPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.32584F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.67416F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(594, 359);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(236, 326);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(355, 30);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.Controls.Add(this.radioButton1);
+            this.flowLayoutPanel2.Controls.Add(this.ManuallyPanel);
+            this.flowLayoutPanel2.Controls.Add(this.radioButton2);
+            this.flowLayoutPanel2.Controls.Add(this.radioButton3);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(227, 317);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(137, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Select Images Manually";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // ManuallyPanel
+            // 
+            this.ManuallyPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ManuallyPanel.Controls.Add(this.AddLogoBox);
+            this.ManuallyPanel.Controls.Add(this.opacityValue);
+            this.ManuallyPanel.Controls.Add(this.OpacityValueLable);
+            this.ManuallyPanel.Controls.Add(this.logoImage);
+            this.ManuallyPanel.Controls.Add(this.clearButton);
+            this.ManuallyPanel.Controls.Add(this.showButton);
+            this.ManuallyPanel.Location = new System.Drawing.Point(3, 26);
+            this.ManuallyPanel.Name = "ManuallyPanel";
+            this.ManuallyPanel.Size = new System.Drawing.Size(214, 142);
+            this.ManuallyPanel.TabIndex = 3;
+            this.ManuallyPanel.Visible = false;
+            // 
+            // opacityValue
+            // 
+            this.opacityValue.Location = new System.Drawing.Point(94, 87);
+            this.opacityValue.Name = "opacityValue";
+            this.opacityValue.Size = new System.Drawing.Size(100, 20);
+            this.opacityValue.TabIndex = 5;
+            this.opacityValue.Text = "30";
+            this.opacityValue.Visible = false;
+            this.opacityValue.TextChanged += new System.EventHandler(this.opacityValue_TextChanged);
+            // 
+            // OpacityValueLable
+            // 
+            this.OpacityValueLable.AutoSize = true;
+            this.OpacityValueLable.Location = new System.Drawing.Point(6, 90);
+            this.OpacityValueLable.Name = "OpacityValueLable";
+            this.OpacityValueLable.Size = new System.Drawing.Size(82, 13);
+            this.OpacityValueLable.TabIndex = 4;
+            this.OpacityValueLable.Text = "Opacity of Logo";
+            this.OpacityValueLable.Visible = false;
+            // 
+            // logoImage
+            // 
+            this.logoImage.AutoSize = true;
+            this.logoImage.Location = new System.Drawing.Point(3, 55);
+            this.logoImage.Name = "logoImage";
+            this.logoImage.Size = new System.Drawing.Size(134, 23);
+            this.logoImage.TabIndex = 3;
+            this.logoImage.Text = "Select Image for logo";
+            this.logoImage.UseVisualStyleBackColor = true;
+            this.logoImage.Visible = false;
+            this.logoImage.Click += new System.EventHandler(this.logoImage_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.AutoSize = true;
+            this.clearButton.Location = new System.Drawing.Point(6, 113);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(94, 23);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Convert To PDF";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.AutoSize = true;
+            this.showButton.Location = new System.Drawing.Point(3, 3);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(182, 23);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "Select Images to convert it into Pdf";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 174);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(119, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Select Single Folder";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 197);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(126, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Select Multiple Folder";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Select a picture file";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // AddLogoBox
+            // 
+            this.AddLogoBox.AutoSize = true;
+            this.AddLogoBox.Location = new System.Drawing.Point(6, 32);
+            this.AddLogoBox.Name = "AddLogoBox";
+            this.AddLogoBox.Size = new System.Drawing.Size(72, 17);
+            this.AddLogoBox.TabIndex = 1;
+            this.AddLogoBox.Text = "Add Logo";
+            this.AddLogoBox.UseVisualStyleBackColor = true;
+            this.AddLogoBox.CheckedChanged += new System.EventHandler(this.AddLogoBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 359);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ManuallyPanel.ResumeLayout(false);
+            this.ManuallyPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel ManuallyPanel;
+        private System.Windows.Forms.Label OpacityValueLable;
+        private System.Windows.Forms.Button logoImage;
+        private System.Windows.Forms.TextBox opacityValue;
+        private System.Windows.Forms.CheckBox AddLogoBox;
+    }
+}
+
---
+++ ImagetoPdf/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Web.UI.WebControls;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace ImagetoPdf
+{
+    public partial class Form1 : Form
+    {
+        float opacityVal = 0.3f;
+        List<string> files = new List<string>();
+        private string watermarkImagePath = null; 
+        bool logoNeeded = false;
+        bool opacityVal_message_shown = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //watermarkImagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "TempleLogo.png");
+        }
+
+        
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Show the Open File dialog. If the user clicks OK, load the
+            // picture that the user chose.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                   files.Add(file);
+                   
+                }
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            using (PdfSharp.Pdf.PdfDocument pdfDocument = new PdfSharp.Pdf.PdfDocument())
+            {
+                if (!files.Any())
+                {
+                    MessageBox.Show("Images not found. Please Select Images to convert.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                else
+                {
+                    foreach (string imageFile in files)
+                    {
+                        PdfPage page = pdfDocument.AddPage();
+                        using (XGraphics gfx = XGraphics.FromPdfPage(page))
+                        {
+                            using (XImage img = XImage.FromFile(imageFile))
+                            {
+                                page.Width = img.PixelWidth * 72 / img.HorizontalResolution;
+                                page.Height = img.PixelHeight * 72 / img.VerticalResolution;
+                                gfx.DrawImage(img, 0, 0, page.Width, page.Height);
+
+                                if (string.IsNullOrEmpty(watermarkImagePath))
+                                {
+                                    if (logoNeeded)
+                                    {
+                                    MessageBox.Show("Watermark image not found. Please check the path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                    }
+                                }
+                                else
+                                {
+                                    // Add watermark
+                                    DrawWatermark(gfx, page);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        pdfDocument.Save(saveFileDialog.FileName);
+                        MessageBox.Show("PDF created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+
+        private void DrawWatermark(XGraphics gfx, PdfPage page)
+        {
+            
+            if (opacityVal <= 0f || opacityVal >100)
+            {
+                MessageBox.Show("Opacity Value not found. Please provide value for Opacity.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                using (Bitmap bitmap = new Bitmap(watermarkImagePath))
+                {
+                    using (Bitmap semiTransparentBitmap = ApplyOpacity(bitmap, opacityVal)) // 30% opacity
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            semiTransparentBitmap.Save(ms, ImageFormat.Png);
+                            ms.Position = 0;
+                            using (XImage watermarkImage = XImage.FromStream(ms))
+                            {
+                                double width = page.Width * 0.1; // Watermark width (30% of page width)
+                                double height = watermarkImage.PixelHeight * width / watermarkImage.PixelWidth; // Maintain aspect ratio
+                                double x = page.Width - width - 10; // 10 points margin from the right
+                                double y = page.Height - height - 10; // 10 points margin from the bottom
+
+                                gfx.DrawImage(watermarkImage, x, y, width, height);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private Bitmap ApplyOpacity(Bitmap bitmap, float opacity)
+        {
+            
+            Bitmap transparentBitmap = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics g = Graphics.FromImage(transparentBitmap))
+            {
+                ColorMatrix colorMatrix = new ColorMatrix
+                {
+                    Matrix33 = opacity // Set the opacity level
+                };
+                ImageAttributes imageAttributes = new ImageAttributes();
+                imageAttributes.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+                g.DrawImage(bitmap, new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                    0, 0, bitmap.Width, bitmap.Height, GraphicsUnit.Pixel, imageAttributes);
+            }
+
+            return transparentBitmap;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                ManuallyPanel.Visible = true;
+            }
+            else
+            {
+                ManuallyPanel.Visible = false;
+            }
+        }
+
+        private void logoImage_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                watermarkImagePath = openFileDialog1.FileName;
+            }
+        }
+
+        private void opacityValue_TextChanged(object sender, EventArgs e)
+        {
+            if(opacityValue.Text!="")
+                opacityVal = float.Parse(opacityValue.Text)/100;
+            else
+            {
+                opacityVal = 0;
+            }
+        }
+
+        private void AddLogoBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (AddLogoBox.Checked)
+            {
+                logoImage.Visible = true;
+                OpacityValueLable.Visible = true;
+                opacityValue.Visible = true;
+                logoNeeded = true;
+            }
+            else
+            {
+                logoImage.Visible = false;
+                OpacityValueLable.Visible = false;
+                opacityValue.Visible = false;
+                logoNeeded = false;
+            }
+        }
+    }
+
+
+}
